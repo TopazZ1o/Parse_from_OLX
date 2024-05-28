165027509374bbd3d7447acc668dcd38e6e44998
Topazzio <tima.makhanov@mail.ru>
2024-05-28 21:06:16 +0500

    Beta (without views 0.2)

--- views.py
+++ views.py
@@ -1,9 +1,62 @@
+import json
 from requests import Session
 
 link = "https://production-graphql.eu-sharedservices.olxcdn.com/graphql"
 
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ru",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Content-Type": "application/json",
+    "apollo-require-preflight": "true",
+}
+
+
+def main(link):
+    s = Session()
+    s.headers.update(headers)
+
+    query = {
+        "query": """
+        query {
+          myAds {
+            pageViews {
+              pageViews
+              __typename
+            }
+            __typename
+          }
+        }
+        """
+    }
+
+    response = s.post(link, json=query)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Extract the number of page views
+        page_views = data.get("data", {}).get("myAds", {}).get("pageViews", {}).get("pageViews", None)
+
+        print(f"Page Views: {page_views}")
+
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+    else:
+        print(f"Query failed to run with status code {response.status_code}")
+        print(f"Response: {response.text}")
+
+
+main(link)
+
+"""
+from requests import Session
+
+link = "https://production-graphql.eu-sharedservices.olxcdn.com/graphql"
+
+headers = {
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ru",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     "Content-Type": "application/json",
     "apollo-require-preflight": "true",
 }
@@ -18,3 +71,4 @@ def main(link):
 
 
 main(link)
+"""
